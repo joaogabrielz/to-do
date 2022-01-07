336bbefb84461e74dee778a23f038ba456540ed5
joaogabrielz <zacarias1021@gmail.com>
2022-01-07 12:19:23 -0400

    melhorias no codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,43 @@
 const button = document.getElementById('btn');
-button.addEventListener('click', tarefa);
+button.addEventListener('click', add);
 
-const textoV = document.getElementById('textoV')
-
+let tarefas =[]
 let cont = 0;
 let timeSumir;
-const postit = document.getElementsByClassName('container')[0]
-function tarefa(){ 
-    let div = document.createElement('div')
-    let chek = document.createElement('input')
-    let label = document.createElement('label')
 
-    postit.appendChild(div)
-    div.appendChild(chek)
-    div.appendChild(label)
-   div.style.maxWidth = '300px'
 
-    if(textoV.value.length > 20){
-        label.innerText = `${textoV.value}\n`
-    }
-    else{
-        label.innerText = textoV.value
-    }
+function add(){
+   
+    const postit = document.getElementsByClassName('container')[0]
+    let descricao = document.getElementById('textoV').value
+    tarefas.push(descricao)
+   
+     let div = document.createElement('div')
+     let chek = document.createElement('input')
+     let label = document.createElement('label')
 
-    chek.type = 'checkbox'
-    textoV.value = ''
+     postit.appendChild(div)
+     div.appendChild(chek)
+     div.appendChild(label)
+     div.style.maxWidth = '300px'
+
+    // label.innerText = descricao
+        if(textoV.value.length > 20){
+            label.innerText = descricao +'\n'
+            }
+        else{
+            label.innerText = descricao
+            }
+        
+      chek.type = 'checkbox'
+      textoV.value = ''
+       
 
     chek.addEventListener('change', risc)
 
-    if(label.innerText == ''){
 
+    if(descricao == ''){
+    tarefas.pop()
     label.innerText = textoV.value
     chek.style.display = 'none'
     }
@@ -40,7 +48,7 @@ function tarefa(){
         button.style.display = 'none'
     }
   
-        function risc(){
+function risc(){
     if(chek.checked == true){
             label.classList.add('label-risc');
             timeSumir = setTimeout(sumir, 2000);
@@ -50,10 +58,73 @@ function tarefa(){
         clearTimeout(timeSumir);
         }
     }
-    function sumir(){
+
+function sumir(){
         cont -= 1;
         label.classList.remove('label-risc');
         label.innerText = ''
+        tarefas.pop()
         chek.style.display = 'none'
-    }
+            }
 }
+//codigo antigo
+
+// const button = document.getElementById('btn');
+// button.addEventListener('click', tarefa);
+
+// const textoV = document.getElementById('textoV')
+
+// let cont = 0;
+// let timeSumir;
+// const postit = document.getElementsByClassName('container')[0]
+// function tarefa(){ 
+//     let div = document.createElement('div')
+//     let chek = document.createElement('input')
+//     let label = document.createElement('label')
+
+//     postit.appendChild(div)
+//     div.appendChild(chek)
+//     div.appendChild(label)
+//    div.style.maxWidth = '300px'
+
+//     if(textoV.value.length > 20){
+//         label.innerText = `${textoV.value}\n`
+//     }
+//     else{
+//         label.innerText = textoV.value
+//     }
+
+//     chek.type = 'checkbox'
+//     textoV.value = ''
+
+//     chek.addEventListener('change', risc)
+
+//     if(label.innerText == ''){
+
+//     label.innerText = textoV.value
+//     chek.style.display = 'none'
+//     }
+//     else{
+//     cont += 1;
+//     }
+//     if(cont >= 10){
+//         button.style.display = 'none'
+//     }
+  
+//         function risc(){
+//     if(chek.checked == true){
+//             label.classList.add('label-risc');
+//             timeSumir = setTimeout(sumir, 2000);
+//     }
+//     if(chek.checked == false) {
+//         label.classList.remove('label-risc');
+//         clearTimeout(timeSumir);
+//         }
+//     }
+//     function sumir(){
+//         cont -= 1;
+//         label.classList.remove('label-risc');
+//         label.innerText = ''
+//         chek.style.display = 'none'
+//     }
+// }
